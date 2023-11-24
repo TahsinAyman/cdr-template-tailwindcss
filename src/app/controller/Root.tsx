64b6bcdb6bbfe3eab240dce500236ba7e1ece035
fullstackltd <fullstackltd@gmail.com>
2023-11-24 12:21:51 +0600

    docs

--- src/app/controller/Root.tsx
+++ src/app/controller/Root.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import RootView from "../views/RootView";
 
 export default function Root() {
-  return (
-    <RootView />
-  )
+  const [count, setCount] = useState<number>(0);
+
+  const increment = (): void => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const data = {
+    count,
+  };
+  const events = {
+    increment
+  };
+
+  return <RootView data={data} events={events} />;
 }
--- src/app/views/RootView.tsx
+++ src/app/views/RootView.tsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
-import reactLogo from "src/resources/assets/images/react.svg";
-import viteLogo from "/vite.svg";
 import "./css/style.css";
 
-export default function Root() {
-  const [count, setCount] = useState(0);
+interface Props {
+  data: {
+    count: number;
+  },
+  events: {
+    increment: () => void;
+  }
+}
 
+export default function Root(props: Props) {
+  
   return (
     <>
       <h1>Component Driven Redux</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={props.events.increment}>
+          count is {props.data.count}
         </button>
 
         <p className="mt-10">
           Edit <code>src/app/views/RootView.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-
-      </p>
     </>
   );
 }
